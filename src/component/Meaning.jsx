c3d162cf957596e7b3ed88f6f1d04f6367166cdc
striker <goodluckodemilin@gmail.com>
2023-06-30 21:47:57 +0100

    fixed homepage styles

--- src/component/Meaning.jsx
+++ src/component/Meaning.jsx
@@ -1,50 +1,60 @@
-import React from 'react'
-import video from "../media/what-effect.mp4"
+import React from "react";
+import video from "../media/what-effect.mp4";
 export default function Meaning() {
   return (
     <div className="meaning">
+      <iframe
+        className="video1"
+        src="https://www.youtube.com/embed/_Bjru0-eApE?autoplay=0&mute=1"
+        width="600"
+        height="300"
+        frameBorder="0"
+        alt="intro video"
+      ></iframe>
 
-        <iframe className="video1" src="https://www.youtube.com/embed/_Bjru0-eApE?autoplay=0&mute=1" width="600" height="300" frameBorder="0" alt ="intro video"
-        ></iframe>
-    
-        <div className="meaningText">
-            <header id="meaningHeader">
+      <div className="meaningText">
+        <header id="meaningHeader">
+          <h1>What is the CryptoGram</h1>
+          <p className="test animation">
+            Gram is a global Cryptocurrency Ecosystem whose members help each
+            other to achieve financial well-being using our tools and algorithm
+            of instant profit distribution on smart contracts.
+          </p>
+          <p className="test animation">
+            The CrpytoGram system belongs to our Community and is fully
+            decentralized , which means it is transparent, secure and resistant
+            to external influences.
+          </p>
+          <p className="note test animation">
+            A Smart contract is a self-excuting algorithm (program code). it
+            guarantees the transparency and security of executing logic or
+            transfer within the blockchain.
+          </p>
+          <p className="test animation">
+            The platform Gram is open and available to everyone. The process of
+            entering the community and starting to make a profit is so simple
+            that you only need a laptop or a smartphone with an Internet
+            connection. Develop matrix platforms and invite partners to the
+            structure, for which you get remuneration in POLYGON
+            cryptocurrencies.
+          </p>
+          <p className="note test animation">
+            No risk - META FORCE is different from other platforms, transactions
+            are processed in a blockchain by a smart contract and cannot be
+            changed or stopped even by the creators. Transfers go directly to
+            participants' personal wallets with no hidden fees or use of
+            third-party resources. Which means that every reward earned belongs
+            entirely to the individual and can be used immediately.
+          </p>
 
-                <h1>What is the CryptoGram</h1>
-                <p className="test animation">Gram is a global Cryptocurrency Ecosystem whose
-                     members help each other to achieve financial
-                      well-being using our tools and algorithm of
-                     instant profit distribution on smart contracts.</p>
-                    <p className="test animation" >The CrpytoGram system belongs to our Community
-                         and is fully decentralized , which means it is 
-                         transparent,
-                         secure and resistant to external influences.</p>
-                        <p className="note test animation">A Smart contract is a self-excuting 
-                            algorithm (program code). it guarantees the
-                             transparency and security of 
-                            executing logic or transfer within the blockchain.</p>
-                            <p className="test animation">The platform Gram is open and available to everyone. 
-                                The process of entering the community and starting 
-                                to make a profit is so simple that you only need a 
-                                laptop or a smartphone with an Internet connection. 
-                                Develop matrix platforms and invite partners to the
-                                 structure, for 
-                                which you get remuneration in POLYGON cryptocurrencies.</p>
-                                <p className="note test animation">No risk - META FORCE is different from other platforms,
-                                     transactions are processed in a blockchain by a 
-                                     smart contract and cannot be changed or stopped 
-                                     even by the creators. Transfers go directly to 
-                                     participants' personal wallets with no hidden fees
-                                      or use of third-party resources. Which means that
-                                       every reward earned belongs entirely
-                                     to the individual and can be used immediately.
-                                </p>
-                              
-                                    <a className="meaningFooter test animation" href="https://meta-force.space/academy">Learn more with our Partners</a>
-                                
-            </header>
-        </div>
-    
-</div>
-  )
+          <a
+            className="meaningFooter test animation"
+            href="https://meta-force.space/academy"
+          >
+            Learn more with our Partners
+          </a>
+        </header>
+      </div>
+    </div>
+  );
 }
