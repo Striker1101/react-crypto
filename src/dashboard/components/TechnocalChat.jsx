e79fd2956f03ba045b9f84221e38cd61749de0eb
striker <goodluckodemilin@gmail.com>
2023-07-21 03:38:41 +0100

    fixed technical view

--- src/dashboard/components/TechnocalChat.jsx
+++ src/dashboard/components/TechnocalChat.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export default function TechnocalChat({width}) {
+export default function TechnocalChat({ width }) {
   return (
-    <div>
-      <iframe height="510" width={width} src="https://ssltvc.investing.com/?pair_ID=27&height=510&width=1000&interval=300&plotStyle=area&domain_ID=72&lang_ID=72&timezone_ID=0"></iframe>
+    <div className="chat">
+      <iframe
+        height="510"
+        width={width}
+        src="https://ssltvc.investing.com/?pair_ID=27&height=510&width=1000&interval=300&plotStyle=area&domain_ID=72&lang_ID=72&timezone_ID=0"
+      ></iframe>
+      <div className="cover"></div>
     </div>
-  )
+  );
 }
